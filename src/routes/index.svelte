4c28c0f39341e0f8c97a65f4d9bf27ad9e95d2d1
qw-rbx <106570163+OpenSauceRepositories@users.noreply.github.com>
2022-05-31 22:10:19 +0800

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<svelte:head>
+	<title>hmm</title>
+	<meta name="hmm" content="hmm" />
+	<link rel="icon" type="image/png" href="/hmm.png">
+</svelte:head>
+
+<section>
+	<div class="container">
+		<img src="/hmm.svg" alt="hmm" height="500" width="500"/>
+	</div>
+</section>
+
+<style>
+	:global(body) {
+		background-color: rgb(255, 206, 20);
+	}
+	.container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+</style>
